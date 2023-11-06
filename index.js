2dbd110fc6f60c2c21f2979ddd1acd1d0e9d8954
Mahmud Hasan <mahmud.hasan@ymail.com>
2023-11-06 12:16:13 +0600

    all jobs api created

--- index.js
+++ index.js
@@ -40,7 +40,11 @@ async function run() {
             res.send(result);
         })
 
-
+        //  get all jobs
+        app.get('/allJobs', async (req, res) => {
+            const result = await jobsCollections.find().toArray();
+            res.send(result)
+        })
 
         // Send a ping to confirm a successful connection
         // await client.db("admin").command({ ping: 1 });
